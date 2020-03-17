22ba4ddee41f97350475bac1caede9bd624bf228
Temitope Ajiboye <tayormi@gmail.com>
2020-03-17 23:07:11 +0100

    Update Information UI

--- lib/pages/screens/information.dart
+++ lib/pages/screens/information.dart
@@ -1,7 +1,7 @@
 import 'package:covid_tracker/pages/widgets/information_title_card.dart';
 import 'package:covid_tracker/theme/color/light_color.dart';
-import 'package:covid_tracker/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:responsive_screen/responsive_screen.dart';
 
 class InformationScreen extends StatelessWidget {
@@ -10,25 +10,18 @@ class InformationScreen extends StatelessWidget {
     final Function wp = Screen(context).wp;
     final Function hp = Screen(context).hp;
     return Scaffold(
-      backgroundColor: LightColor.background,
-      
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   elevation: 0,
-      //   backgroundColor: LightColor.background,
-      //   title: Text('Information', style: AppTheme.h1Style.copyWith(color: LightColor.darkgrey))
-      // ),
+    
       body: SingleChildScrollView(
         padding: EdgeInsets.only(left: 20, right: 20, top: 20),
         child: Column(
           children: <Widget>[
-            InformationTitleCard(icon: Icons.info, iconColor: Color(0xFFd7d5ed), subTitle: 'Learn how Covid-19 spread', title: 'How it Spreads?',),
+            InformationTitleCard(icon: LineIcons.share_alt, iconColor: CardColors.blue, subTitle: 'Learn how Covid-19 spread', title: 'How it Spreads?',),
             SizedBox(height: hp(3)),
-            InformationTitleCard(icon: Icons.info, iconColor: Color(0xFFfee9e8), subTitle: 'Learn Covid-19 symptoms', title: 'Symptoms',),
+            InformationTitleCard(icon: LineIcons.warning, iconColor: CardColors.cyan, subTitle: 'Learn Covid-19 symptoms', title: 'Symptoms',),
             SizedBox(height: hp(3)),
-            InformationTitleCard(icon: Icons.info, iconColor: Color(0xFFd6eede), subTitle: 'Learn Covid-19 treatments', title: 'Prevention & treatment',),
+            InformationTitleCard(icon: LineIcons.heartbeat, iconColor: CardColors.red, subTitle: 'Learn Covid-19 treatments', title: 'Prevention & treatment',),
             SizedBox(height: hp(3)),
-            InformationTitleCard(icon: Icons.info, iconColor: Color(0xFFe7f5fe), subTitle: 'What to do if you get the virus', title: 'What to do',),
+            InformationTitleCard(icon: LineIcons.question_circle, iconColor:CardColors.green, subTitle: 'What to do if you get the virus', title: 'What to do',),
           ],
         )
       ),
